4e5c8d7d17391a361da2c8b4bf3dc7c0b38d2619
Julien Le Mee <julemee@gmail.com>
2022-10-05 01:21:22 -0400

    custom game

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const cells = document.querySelectorAll('.cell');
+const reset = document.getElementById('reset');
+
 const winningConditions = [
   [0, 1, 2],
   [3, 4, 5],
@@ -12,7 +14,51 @@ const winningConditions = [
 
 cells.forEach((cell) => {
   cell.addEventListener('click', (event) => {
-    console.log(event.target.cellIndex);
-    event.target.classList.toggle('clicked');
+    if (event.target.textContent === '') {
+      event.target.textContent = 'X';
+      event.target.style.color = '#f19a3e';
+    } else if (event.target.textContent === 'X') {
+      event.target.textContent = 'O';
+      event.target.style.color = '#8EDCE6';
+    } else {
+      event.target.textContent = '';
+    }
+    checkWinner();
+    // event.target.classList.add('orange-player');
   });
 });
+
+function checkWinner() {
+  let winner = null;
+  winningConditions.forEach((condition) => {
+    if (
+      cells[condition[0]].textContent === 'X' &&
+      cells[condition[1]].textContent === 'X' &&
+      cells[condition[2]].textContent === 'X'
+    ) {
+      winner = 'X';
+    }
+    if (
+      cells[condition[0]].textContent === 'O' &&
+      cells[condition[1]].textContent === 'O' &&
+      cells[condition[2]].textContent === 'O'
+    ) {
+      winner = 'O';
+    }
+  });
+  if (winner === 'X') {
+    document.getElementById('winner').textContent = 'X is the winner!';
+  } else if (winner === 'O') {
+    document.getElementById('winner').textContent = 'O is the winner!';
+  }
+  else {
+    document.getElementById('winner').textContent = '';
+  }
+}
+
+reset.addEventListener('click', () => {
+  // recharger la page
+  location.reload();
+});
+
+// jouer chacun son tour : X puis O puis X puis O :
